bb7f87f3c70932626d28119444a93c61ee8e6904
ischlag <imanol.schlag@gmail.com>
2025-07-29 22:22:41 +0200

    Remove duplicate license filtering files from decontamination

--- 03-decontamination/run_license_filtering.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-dataset_path="/capstor/store/cscs/swissai/infra01/posttrain_data/02_standardised/Llama-Nemotron-Post-Training-Dataset_wo_math_code"
-python license_filtering.py --dataset_path="${dataset_path}"
